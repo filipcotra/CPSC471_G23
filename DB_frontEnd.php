08ca4404fe07b98d76cf0300cebf217f11950443
theodore-lun <theodore.lun@gmail.com>
2023-04-03 03:18:59 -0600

    login

--- DB_frontEnd.php
+++ DB_frontEnd.php
@@ -1,7 +1,6 @@
 <html>
 
 <head>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <style>
 
 :root {
@@ -9,7 +8,7 @@
 	--columnHeight: 10%;
 }
 
-#table_header table {
+table {
 	border: 1px solid #000000;
 	width: 100%;
 	height: 100%;
@@ -19,67 +18,22 @@
 	text-align: center;
 }
 
-#table_header td {
+td {
 	border: 2px solid #000000;
 	height: 20%;
 	text-align: center;
 }
 
-#table_header thead,th {
+thead,th {
 	height: 5%;
 	border: 2px solid #000000;
 	background-color: #adadad;
 	color:#000000;
 }
 
-#table_header tbody {
+tbody {
 	background-color: #ebecf0;
 	table-layout: fixed;
-}
-
-#table_full table {
-	border: 1px solid #000000;
-	width: 100%;
-	height: 100%;
-	table-layout: fixed;
-	color: #000000;
-	background-color: #ffffff:
-	text-align: center;
-}
-
-#table_full td {
-	border: 2px solid #000000;
-	height: 20%;
-	text-align: center;
-}
-
-#table_full thead,th {
-	height: 5%;
-	border: 2px solid #000000;
-	background-color: #adadad;
-	color:#000000;
-}
-
-#table_full tbody {
-	background-color: #ebecf0;
-	table-layout: fixed;
-}
-
-#table_cols table {
-	border: 1px solid #000000;
-	width: 100%;
-	height: 100%;
-	table-layout: fixed;
-	color: #000000;
-	background-color: #d3d3d3:
-	text-align: center;
-}
-
-#table_cols td {
-	background-color:#d3d3d3;
-	border: 2px solid #000000;
-	height: 20%;
-	text-align: center;
 }
 
 #header {
@@ -96,7 +50,7 @@
 	gap: 0.5%;
 	padding: 0.5%;
 	grid-template-columns: 33% 33% 33%;
-	grid-template-rows: 20% 10% 10% 55% 5%;
+	grid-template-rows: 20% 75%;
 	background-color:powderblue;
 	height: 80%;
 }
@@ -134,28 +88,14 @@
 	padding:10%;
 }
 
-.db_header {
+.db_table {
 	border:3px solid black;
 	grid-row: 2;
 	grid-column: 1/-1;
 }
 
-.db_colNames {
-	background-color:#d3d3d3;
-	border:3px solid black;
-	grid-row: 3;
-	grid-column: 1/-1;
-}
-
-.db_table {
-	background-color:#ffffff;
-	border:3px solid black;
-	grid-row: 4;
-	grid-column: 1/-1;
-}
-
 .searchButton {
-	grid-row: 5;
+	grid-row: 3;
 	grid-column: 2;
 	color: #1c2e4a;
 	display: inline-block;
@@ -163,11 +103,59 @@
 }
 
 .loginButton {
-	grid-row: 5;
+	grid-row: 3;
 	grid-column: 3;
 	color: #1c2e4a;
 	display: inline-block;
 	text-align: center;
+}
+
+.editButton {
+	display: none;
+	grid-row: 3;
+	grid-column: 1;
+	color: #1c2e4a;
+	text-align: center;
+}
+
+.loginPopup {
+  display: none;
+  position: fixed;
+  bottom: 25%;
+  left: 32.5%;
+  z-index: 1;
+}
+
+.login-container {
+  padding: 10px;
+  max-width: 400px;
+  background-color: #1c2e4a;
+}
+
+.login-container input[type=text], .login-container input[type=password] {
+  width: 100%;
+  padding: 5px;
+  margin: 5px 0px 20px 0px;
+}
+
+.login-container input[type=text]:focus, .login-container input[type=password]:focus {
+  background-color: powderblue;
+}
+
+.login-container .btn {
+  background-color:powderblue;
+  color: black;
+  padding: 15px;
+  width: 100%;
+  margin-bottom:10px;
+}
+
+.login-container .close {
+  background-color: darkred;
+}
+
+.login-container .btn:hover {
+  opacity: 0.7;
 }
 
 #chooseEntity {
@@ -221,7 +209,7 @@
 <body>
 
 <div id="header">
-	<h1><font color=#880808> Blood Bank<font color=#000000> Database Management System - CPSC471 G23</h1>
+	<h1>CPSC471 Final Project - G23<font color=#880808> Blood Bank<font color=#000000> Database Management System</h1>
 </div>
 	
 	<br></br>
@@ -231,33 +219,30 @@
 		<label id="chooseEntity">Choose an entity:</label>
 		<select id="entity">
 			<option value="+">-Select an Entity Type-</option>
-			<option value="=,a,b,c,d">administered_to</option>
-			<option value="2">advertiser</option>
-			<option value="3">blood</option>
-			<option value="=,e,f,g,h">blood_bank</option>
-			<option value="5">blood_bank_employee</option>
-			<option value="6">blood_tester</option>
-			<option value="7">delivered_blood_to</option>
-			<option value="8">distributor</option>
-			<option value="9">doctor</option>
-			<option value="10">donates_to</option>
-			<option value="=,i,j,k,l,m,n,o">donor</option>
-			<option value="12">donor_addresses</option>
-			<option value="13">donor_phones</option>
-			<option value="14">donor_technician</option>
-			<option value="15">hospital</option>
-			<option value="16">hospital_employee</option>
-			<option value="17">nurse</option>
-			<option value="18">past_employer_bloodbank</option>
-			<option value="19">past_employer_hospital</option>
-			<option value="20">receptionist</option>
-			<option value="=,i,j,k,l,m,n,o">recipient</option>
-			<option value="22">recipient_addresses</option>
-			<option value="23">recipient_phones</option>
-			<option value="24">sends_results</option>
-			<option value="25">stored_by</option>
-			<option value="26">transferred_blood_to_bank</option>
-			<option value="27">transferred_blood_to_distributor</option>
+			<option value="a,b,c,d">Administration of Blood</option>
+			<option value="2">Advertiser</option>
+			<option value="3">Blood</option>
+			<option value="4">Blood Bank</option>
+			<option value="5">Blood Bank Employees</option>
+			<option value="6">Blood Bank Receptionist</option>
+			<option value="7">Blood Tester</option>
+			<option value="8">Blood Storage</option>
+			<option value="9">Distributor</option>
+			<option value="10">Delivery of Blood</option>
+			<option value="11">Doctor</option>
+			<option value="12">Donation</option>
+			<option value="13">Donation Technician</option>
+			<option value="e,f,g,h,i,j,k">Donor</option>
+			<option value="15">Donor Addresses</option>
+			<option value="16">Donor Contact Information</option>
+			<option value="17">Hospital</option>
+			<option value="18">Hospital Employees</option>
+			<option value="19">Nurse</option>
+			<option value="20">Past Employers of Blood Bank Employees</option>
+			<option value="21">Past Employers of Hospital Employees</option>
+			<option value="e,f,g,h,i,j,k">Recipient</option>
+			<option value="23">Recipient Addresses</option>
+			<option value="24">Recipient Contact Information</option>
 		</select>
 	</div>
 
@@ -266,26 +251,19 @@
 		<select id="attribute">
 			//If default is for some reason re-selected
 			<option value="+">-First Select an Entity-</option>
-			//Once a value is selected - always included
-			<option value="=">-Select an Attribute-</option>
 			//AdministeredTo Attributes
 			<option value="a">Donor Health ID</option>
 			<option value="b">Date of Donation</option>
 			<option value="c">Receipient Health ID</option>
 			<option value="d">Date of Administration</option>
-			//Blood Bank Attributes
-			<option value="e">Name</option>
-			<option value="f">StorageConditions</option>
-			<option value="g">Address</option>
-			<option value="h">PhoneNum</option>
 			//Donor and Recipient Attributes
-			<option value="i">Age</option>
-			<option value="j">FName</option>
-			<option value="k">HealthID</option>
-			<option value="l">LName</option>
-			<option value="m">MName</option>
-			<option value="n">ReceptionistName</option>
-			<option value="o">TechnicianName</option>
+			<option value="e">Age</option>
+			<option value="f">FName</option>
+			<option value="g">HealthID</option>
+			<option value="h">LName</option>
+			<option value="i">MName</option>
+			<option value="j">ReceptionistName</option>
+			<option value="k">TechnicianName</option>
 		</select>
 
 		<label id="chooseAttributeInit">Choose an Attribute:</label>
@@ -299,100 +277,99 @@
 		<input id="enterBox" type="text">
 	</div>
 
-	<div class="db_header" id="table_header">
+	<div class="db_table">
 		<table>
 			<thead>
 				<tr>
-					<th colspan="11">Search Results</th>
+					<th colspan="11">Table Header</th>
 				</tr>
 			</thead>
+			<tbody>
+				<tr>
+					<td>Name 1</td>
+					<td>Name 2</td>
+					<td>Name 3</td>
+					<td>Name 4</td>
+					<td>Name 5</td>
+					<td>Name 6</td>
+					<td>Name 7	</td>
+					<td>Name 8</td>
+					<td>Name 9</td>
+					<td>Name 10</td>
+					<td>Name 11</td>
+				</tr>
+				<tr>
+					<td>Column 1</td>
+					<td>Column 2</td>
+					<td>Column 3</td>
+					<td>Column 4</td>
+					<td>Column 5</td>
+					<td>Column 6</td>
+					<td>Column 7</td>
+					<td>Column 8</td>
+					<td>Column 9</td>
+					<td>Column 10</td>
+					<td>Column 11</td>
+				</tr>
+				<tr>
+					<td>Column 1</td>
+					<td>Column 2</td>
+					<td>Column 3</td>
+					<td>Column 4</td>
+					<td>Column 5</td>
+					<td>Column 6</td>
+					<td>Column 7</td>
+					<td>Column 8</td>
+					<td>Column 9</td>
+					<td>Column 10</td>
+					<td>Column 11</td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
-	
-	<div class="db_colNames" id="table_cols">
-	</div>
-	
-	<div class="db_table" id="table_full">
-	</div>
-	
 	<div class="searchButton">
-		<button type="button" onclick="search()">Search</button>
+		<button type="button">Search</button>
 	</div>
-	
 	<div class="loginButton">
-		<button type="button">Login</button>
+		<button type="button" onclick="openLoginPopup()" >Login</button>
 	</div>
+	<div class="editButton" id="editbtn">
+		<button type="button">Edit</button>
+	</div>
+
+
+	<div class="loginPopup" id="loginForm">
+  		<form action="/action_page.php" class="login-container">
+	    	<h1><font color=white>Login to edit database <font color=white></h1>
+	    	<label for="Username"><b>Username</b></label>
+	    	<input type="text" name="Username" required>
+	    	<label for="pw"><b>Password</b></label>
+	    	<input type="password" name="pw" required>
+	    	<button type="button" class="btn" onclick="showEdit()" >Login</button>
+	    	<button type="button" class="btn close" onclick="closeLoginPopup()">Close</button>
+	    </form>
+	</div>
+
+<script>
+	//script for handling login button
+	//change to type="submit" for php for Login
+	function openLoginPopup() {
+	  document.getElementById("loginForm").style.display = "block";
+	}
+
+	function closeLoginPopup() {
+	  document.getElementById("loginForm").style.display = "none";
+	}
+
+	function showEdit() {
+		closeLoginPopup();
+		document.getElementById("editbtn").style.display="inline-block";
+	}
+</script>
+
 </div>
 
 <script>
-	// Setting fields to hold dropdown menu information
-	var entitySelection = null;
-	var attributeSelection = null;
-	var attributeEntry = null;
-	var queryResult = null;
-//------------------------- Desc -------------------------------------//
-// This method is to update the display table based on a search result.
-// It will do so by updating the db_table div.
-//------------------------- Code -------------------------------------//
-	// Getting the db_table div so that it can be updated. Also setting
-	// a variable to track the new innerHTML to be inserted.
-	const tableDiv = document.getElementById('table_full');
-	var tableInner = ' ';
-	const tableColDiv = document.getElementById('table_cols');
-	var colInner = ' ';
-	// This function will make a new table from the search results.
-	// Search results will be a 2d array.
-	function createTable(searchResults){
-		tableInner = ' ';
-		colInner = ' ';
-		tableInner += "<table>";
-		colInner += "<table>";
-		colInner += "<tr>";
-		// Making rows based on searchResults length.
-		for(let i = 0; i < searchResults.length; i++){
-			tableInner += "<tr>";
-			// Making columns based on length of each searchResults
-			// result. These should be constant.
-			for(let j = 0; j < Object.keys(searchResults[i]).length/2; j++){
-				// Adding a column containing the search result attribute.
-				tableInner += "<td>";
-				tableInner += searchResults[i][j];
-				tableInner += "</td>";
-				// Adding the key to the header.
-				colInner += "<td>";
-				colInner += Object.keys(searchResults[i])[j + Object.keys(searchResults[i]).length/2];
-				colInner += "</td>";
-			}
-			tableInner += "</tr>";
-		}
-		tableInner += "</table>";
-		colInner += "</tr>";
-		colInner += "</table>";
-		tableDiv.innerHTML = tableInner;
-		tableColDiv.innerHTML = colInner;
-	}
-//------------------------- Desc -------------------------------------//
-// This method is to search the database. I will only be implementing test
-// values for now. Somebody else should implement an sql SELECT to search for the
-// actual DB results.
-//------------------------- Code -------------------------------------//
-	// Test value
-	function search(){	
-		$.ajax({
-			method: 'POST',
-			url: 'runSelect.php',
-			data: { par1: entitySelection, par2: attributeSelection, par3: attributeEntry },
-			success: function(data){
-				createTable(data);
-			},
-			dataType:"json"
-		});	
-		//createTable(runSelect(entitySelection, attributeSelection, attributeEntry));
-	}
-//------------------------- Desc -------------------------------------//
-// This method is to update the drop down menus so that they will only 
-// show the appropriate attributes after an entity type is selected.
-//------------------------- Code -------------------------------------//
 	// Setting boolean flag, as the first part of event handler below
 	// should only occur once
 	var isFirst = true;
@@ -401,14 +378,12 @@
 	const attr = document.getElementById('attribute');
 	const attrOptions = [...attr.children];
 	const attrLabel = document.getElementById('chooseAttribute');
-	const entityDropdown = document.getElementById('entity');
-	const enterAttr = document.getElementById('enterBox');
 
 	const attr2 = document.getElementById('attributeInit');
 	const attr2Label = document.getElementById('chooseAttributeInit');
 
 	// Adding event handler to dynamically update the dropdown menus
-	entityDropdown.addEventListener(
+	document.getElementById('entity').addEventListener(
   		'change',
   		(e) => {
 			if (isFirst){
@@ -425,33 +400,16 @@
 				// Set flag to false so this won't happen again
 				isDone = false;
 			}
-			entitySelection = entity.options[entity.selectedIndex].text;
-			if(entitySelection == "-Select an Entity Type-"){
-				entitySelection = null;
-			}
 			//Update dropdown elements
 			attr.innerHTML = attrOptions.filter(
       			option => event.target.value.includes(option.value)
     			).map(option => option.outerHTML).join('');
-  		}
-	)
-	// Adding event handlers to collect other info
-	attr.addEventListener(
-		'change',
-		(e) => {
-			attributeSelection = attr.options[attr.selectedIndex].text;
-			if(attributeSelection == "-Select an Attribute-"){
-				attributeSelection = null;
-			}
-		}
-	)
-	enterAttr.addEventListener(
-		'input',
-		(e) => {
-			attributeEntry = enterAttr.text;
-		}
-	)
+  		})
+
+	document.openLoginPage().addEventListener(
+		)
 </script>
+
 
 </body>
 </html>
